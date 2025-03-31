b94ddb7bad02035c1862261c4e85be0753a9d5dd
dbstj0403 <wcn09088@naver.com>
2025-03-31 21:39:28 +0900

    Init: 파일 절대경로 설정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true, // 브라우저 자동 오픈
+    open: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+    },
   },
 });
