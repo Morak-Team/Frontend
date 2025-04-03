80a7e0c0868857b8c43e133c95619ba0d1aca60e
dbstj0403 <wcn09088@naver.com>
2025-04-03 23:40:26 +0900

    Chore: Husky 테스트

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <h1 className="text-3xl text-blue-500">Tailwind 테스트!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl text-blue-500">Tailwind 테스트!</h1>
+      <h2>Home page입니다.</h2>
+    </>
+  );
 };
 
 export default Home;
