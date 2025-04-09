414cce8c3a2324eebd2f64df719e9ddcb14b06fb
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-04-09 18:07:05 +0900

    Chore: 사회적기업 샘플 데이터 추가

---
+++ src/constants/map/socialEnterprise.js
@@ -0,0 +1,24 @@
+const samplePlaces = [
+  {
+    id: 1,
+    name: "아름다운가게 서울본부",
+    category: "사회적기업",
+    distance: "541m",
+    address: "방문자 리뷰 27",
+    images: ["/images/sample1.jpeg", "/images/sample2.jpeg"],
+    liked: true,
+    coords: { lat: 37.632615, lng: 127.015219 },
+  },
+  {
+    id: 2,
+    name: "두바퀴희망자전거협동조합",
+    category: "자전거",
+    distance: "226m",
+    address: "방문자 리뷰 19",
+    images: ["/images/sample3.jpeg", "/images/sample4.jpeg"],
+    liked: true,
+    coords: { lat: 37.535527, lng: 126.970331 },
+  },
+];
+
+export default samplePlaces;
