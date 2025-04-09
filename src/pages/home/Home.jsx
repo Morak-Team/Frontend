9741b94c6e40d1b7862c3d715d3a70d9fa8d1ac1
dbstj0403 <wcn09088@naver.com>
2025-04-09 09:27:17 +0900

    Feat: 홈 페이지에 마이 페이지 이동 버튼 생성

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -21,6 +21,13 @@ const Home = () => {
         >
           지도 페이지로 이동
         </Link>
+
+        <Link
+          to="/myPage"
+          className="px-4 py-2 bg-pink-300 rounded-lg text-center text-white font-semibold shadow-md hover:bg-green-600 transition"
+        >
+          마이 페이지로 이동
+        </Link>
       </div>
     </div>
   );
