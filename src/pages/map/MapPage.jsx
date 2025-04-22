3b5165d19b1b5e35c408d34da36ccfc41dc1f4be
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-04-22 18:57:23 +0900

    Feat: 지도 상단에 카테고리 바 구현

--- src/pages/map/MapPage.jsx
+++ src/pages/map/MapPage.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import MapViewer from "@pages/map/components/MapViewer";
 import PlaceBottomSheet from "./components/PlaceBottomSheet";
 import samplePlaces from "@constants/map/socialEnterprise";
+import CategoryBar from "./components/CategoryBar";
 
 const MapPage = () => {
   const [selectedPlace, setSelectedPlace] = useState(null);
@@ -39,17 +40,22 @@ const MapPage = () => {
         <img src="/svgs/ic_Search.svg" alt="검색 아이콘" className="w-5 h-5" />
       </div>
 
+      {/* 카테고리 바 */}
+      <CategoryBar
+        onSelect={(category) => {
+          // 카테고리 선택 시 필터링 로직
+          const filtered = samplePlaces.filter((p) => p.category === category);
+          setFilteredPlaces(filtered);
+        }}
+      />
+
       {/* 현위치 버튼 */}
       <button
         onClick={() => setMoveToCurrentLocation(true)}
         className="fixed bottom-40 right-8 z-50 w-20 h-20 bg-white rounded-full shadow-md flex items-center justify-center"
       >
-        <img
-          src="/svgs/ic_location.svg"
-          alt="현재 위치 버튼"
-        />
+        <img src="/svgs/ic_location.svg" alt="사용자 현재 위치 버튼" />
       </button>
-
       <MapViewer
         places={filteredPlaces}
         onMarkerClick={setSelectedPlace}
@@ -57,7 +63,6 @@ const MapPage = () => {
         moveToCurrentLocation={moveToCurrentLocation}
         onMoveComplete={() => setMoveToCurrentLocation(false)}
       />
-
       {selectedPlace && (
         <PlaceBottomSheet
           place={selectedPlace}
