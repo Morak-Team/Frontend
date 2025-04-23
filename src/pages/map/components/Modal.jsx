5f17b05f0d0889dcbc723d0e76111cb6ca7d1b80
dbstj0403 <wcn09088@naver.com>
2025-04-23 20:16:59 +0900

    Refactor: 사용하지 않는 Import 제거 및 이미지 경로 수정

--- src/pages/map/components/Modal.jsx
+++ src/pages/map/components/Modal.jsx
@@ -1,16 +1,14 @@
-import { useEffect, useState } from "react";
-
 const Modal = ({ onClose }) => {
   return (
     <div className="absolute bottom-28 left-1/2 -translate-x-1/2 z-[10001] bg-white rounded-xl shadow-xl px-6 py-8 w-80 h-60 flex flex-col justify-between">
       <div className="w-full flex justify-between items-start">
         <img
-          src="/public/images/review/reciptIcon.png"
+          src="/images/review/reciptIcon.png"
           className="w-[60px] h-[60px]"
         />
         <button onClick={onClose} className="p-1 relative bottom-4 left-3">
           <img
-            src="/public/images/review/xButtonIconForModal.png"
+            src="/images/review/xButtonIconForModal.png"
             className="w-6 h-6"
             alt="닫기"
           />
