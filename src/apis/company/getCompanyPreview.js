f775edbfb39d520372fbf84d2135a9b043c30b2e
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-05-02 20:50:48 +0900

    Refactor: 오류 처리 로직 추가

--- src/apis/company/getCompanyPreview.js
+++ src/apis/company/getCompanyPreview.js
@@ -1,8 +1,13 @@
 import api from "@apis/instance/api";
 
 export const getCompanyPreview = async (companyId) => {
-  const response = await api.get("/company/public/preview", {
-    params: { companyId },
-  });
-  return response.data;
+  try {
+    const response = await api.get("/company/public/preview", {
+      params: { companyId },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("회사 미리보기 데이터를 가져오는 중 오류 발생:", error);
+    throw error;
+  }
 };
