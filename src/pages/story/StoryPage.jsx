216418b9f78ed7c5178c20062c83e667cb417313
dbstj0403 <wcn09088@naver.com>
2025-05-06 12:31:37 +0900

    Design: 베스트 스토리 swiper 퍼블리싱 완료

--- src/pages/story/StoryPage.jsx
+++ src/pages/story/StoryPage.jsx
@@ -5,10 +5,10 @@ import StoryCarousel from "@/pages/story/components/carousel/StoryCarousel";
 const StoryPage = () => {
   // 카테고리 캐러셀에 넘겨줄 콜백함수를 지정하고 state를 여기서 관리하면 깔끔할 것 같습니당.
   return (
-    <div className="pb-[8.4rem] pl-[3rem] pr-[3rem]">
-      <h1 className="text-3xl font-bold mt-10 ml-10">베스트 스토리</h1>
+    <div className="h-[calc(100vh-5.25rem)] overflow-y-auto scrollbar-hide p-5">
+      <h1 className="h3 mt-10">많은 응원을 받은 이야기</h1>
       <BestStoryCarousel />
-      <h1 className="text-3xl font-bold mt-10 ml-10">카테고리별 스토리</h1>
+      <h1 className="h3 mt-10">최근 올라온 스토리</h1>
       <CategoryCarousel />
       <StoryCarousel />
     </div>
--- src/pages/story/components/content/SlideContent.jsx
+++ src/pages/story/components/content/SlideContent.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-const SlideContent = ({ title, img, author, num }) => {
+const SlideContent = ({ title, img, author, num, likes = 237 }) => {
   const navigate = useNavigate();
   const moveToDetailPage = () => {
     navigate(`/story/${num}`);
@@ -8,12 +8,33 @@ const SlideContent = ({ title, img, author, num }) => {
 
   return (
     <div
-      className="ml-10 mt-10 flex items-center flex-col"
+      className="relative w-full h-64 rounded-xl overflow-hidden cursor-pointer"
       onClick={moveToDetailPage}
     >
-      <h1>{title}</h1>
-      <img src={img} />
-      <h3>작가 : {author}</h3>
+      {/* 배경 이미지 */}
+      <img src={img} alt={title} className="w-full h-full object-cover" />
+
+      {/* 상단 넘버링 */}
+      <div className="absolute top-2 left-2 text-white text-sm font-semibold drop-shadow-sm">
+        {num}
+      </div>
+
+      {/* 좋아요 + 텍스트 박스 */}
+      <div className="absolute bottom-4 left-0 w-full px-4 pt-6 pb-4 text-white">
+        {/* 좋아요 아이콘 + 숫자 */}
+        <div className="flex items-center gap-1 mb-1">
+          <button className="flex gap-1 bg-white text-primary-8 b4 rounded-md px-1 py-0.5 justify-center items-center">
+            <img
+              src="/svgs/story/storyFireIcon.svg"
+              className="w-4 h-4"
+              alt="like"
+            />
+            <span className="text-xs ">{likes}</span>
+          </button>
+        </div>
+        {/* 제목 */}
+        <div className="h2 text-white mt-4">{title}</div>
+      </div>
     </div>
   );
 };
