da5280a64aab977827f15ae7465623649c643199
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-05-08 01:35:23 +0900

    Feat: 오류 처리 모달 구현

--- src/apis/story/queries.js
+++ src/apis/story/queries.js
@@ -30,14 +30,5 @@ export const usePatchStoryLike = (storyId) => {
         };
       });
     },
-    onError: (error) => {
-      if (error?.response?.status === 401) {
-        alert("로그인이 필요합니다.");
-      } else if (error?.response?.status === 400) {
-        alert("이미 응원하신 스토리입니다.");
-      } else {
-        alert("응원 요청이 실패했습니다. 다시 시도해주세요.");
-      }
-    },
   });
 };
