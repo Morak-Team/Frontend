b5b899c6ec2fcd8c68aef02f3f7373d3e0ac737f
dbstj0403 <wcn09088@naver.com>
2025-05-12 11:35:35 +0900

    Refactor: 불필요한 파일 및 폴더 제거

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import LayoutWithTab from "@/components/layout/LayoutWithTab";
 
-import Home from "@pages/home/Home";
 import KakaoAuth from "@pages/kakaoAuth/kakaoAuth";
 import MapPage from "@pages/map/MapPage";
 import MyPage from "@pages/myPage/MyPage";
