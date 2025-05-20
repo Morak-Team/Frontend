dfc44c2461950e81cfa460119b8e817b8df6698a
dbstj0403 <wcn09088@naver.com>
2025-05-20 16:25:17 +0900

    Fix: 주석 정리

--- src/pages/myPageDetail/MyPageDetailPage.jsx
+++ src/pages/myPageDetail/MyPageDetailPage.jsx
@@ -36,39 +36,13 @@ const MyPageDetailPage = () => {
       </div>
 
       {kind === "리뷰" && (
-        <>
-          {!isLoadingReviews && reviewsData?.length > 0 ? (
-            reviewsData?.map((item, idx) => (
-              <ReviewItem data={item} key={idx} />
-            ))
-          ) : !isLoadingReviews && reviewsData?.length === 0 ? (
-            <div className="flex flex-col justify-center items-center mt-36">
-              <img src={noResult} />
-              <p className="h4 text-gray-9 text-center py-8">
-                아직 작성한 리뷰가
-                <br /> 없어요
-              </p>
-            </div>
-          ) : null}
-        </>
-      )}
-
-      {kind === "응원" && (
-        <>
-          {!isLoadingCheers && cheersData?.length > 0 ? (
-            cheersData?.map((item) => (
-              <StoryItem item={item} key={item.storyId} />
-            ))
-          ) : !isLoadingCheers && cheersData?.length === 0 ? (
-            <div className="flex flex-col justify-center items-center mt-36">
-              <img src={noResult} />
-              <p className="h4 text-gray-9 text-center py-8">
-                아직 응원한 이야기가
-                <br /> 없어요
-              </p>
-            </div>
-          ) : null}
-        </>
+        <div className="flex justify-between items-center px-5">
+          <div className="flex gap-2 items-center my-2">
+            <img src={review} className="w-8 h-8" />
+            <p className="h3">내가 작성한 리뷰</p>
+          </div>
+          <p className="b5 text-gray-9">총 {reviewsData?.length}개</p>
+        </div>
       )}
 
       {kind === "찜" && (
@@ -81,14 +55,24 @@ const MyPageDetailPage = () => {
         </div>
       )}
 
+      {kind === "응원" && (
+        <div className="flex justify-between items-center px-5">
+          <div className="flex gap-2 items-center my-2">
+            <img src={cheer} className="w-8 h-8" />
+            <p className="h3">내가 응원한 이야기</p>
+          </div>
+          <p className="b5 text-gray-9">총 {cheersData?.length}개</p>
+        </div>
+      )}
+
       <div className="flex flex-col gap-4 w-full bg-gray-2 p-5 min-h-screen">
         {kind === "리뷰" && (
           <>
-            {reviewsData?.length > 0 ? (
+            {!isLoadingReviews && reviewsData?.length > 0 ? (
               reviewsData.map((item, idx) => (
                 <ReviewItem data={item} key={idx} />
               ))
-            ) : (
+            ) : !isLoadingReviews && reviewsData?.length === 0 ? (
               <div className="flex flex-col justify-center items-center mt-36">
                 <img src={noResult} />
                 <p className="h4 text-gray-9 text-center py-8">
@@ -96,17 +80,17 @@ const MyPageDetailPage = () => {
                   <br /> 없어요
                 </p>
               </div>
-            )}
+            ) : null}
           </>
         )}
 
         {kind === "응원" && (
           <>
-            {cheersData?.length > 0 ? (
+            {!isLoadingCheers && cheersData?.length > 0 ? (
               cheersData.map((item) => (
                 <StoryItem item={item} key={item.storyId} />
               ))
-            ) : (
+            ) : !isLoadingCheers && cheersData?.length === 0 ? (
               <div className="flex flex-col justify-center items-center mt-36">
                 <img src={noResult} />
                 <p className="h4 text-gray-9 text-center py-8">
@@ -114,7 +98,7 @@ const MyPageDetailPage = () => {
                   <br /> 없어요
                 </p>
               </div>
-            )}
+            ) : null}
           </>
         )}
       </div>
